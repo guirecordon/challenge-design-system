1c2451f38019f5b620609962f7b66aa1924d37f0
Gui Recordon <guilherme.recordon@gmail.com>
2022-10-17 18:19:32 -0300

    set up monorepo

---
+++ packages/react/dist/index.mjs
@@ -0,0 +1,13 @@
+// ../tokens/dist/index.mjs
+var colors = {
+  white: "#FFF",
+  black: "#000",
+  lightgreen: "#E5FCC2",
+  green: "#9DE0AD",
+  blue: "#45ADA8",
+  darkblue: "#547980",
+  gray: "#594F4F"
+};
+
+// src/index.ts
+console.log(colors);
